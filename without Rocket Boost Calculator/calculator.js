fa6295ec1bf84be77af7f20d29506d6d2ec8865f
GustavMair <110348084+GustavMair@users.noreply.github.com>
2024-11-10 22:26:51 +0100

    Add files via upload

---
+++ without Rocket Boost Calculator/calculator.js
@@ -0,0 +1,5 @@
+let rockets= Number(prompt("Hello! How many Rockets do you have?"));
+let generatedStuff = Number(prompt("How much do you generate of the Reccource?"))
+let rocketsTo10 = rockets * 10
+let endResult = generatedStuff / rocketsTo10
+document.write("Okay! Your unboosted value is " + endResult)
